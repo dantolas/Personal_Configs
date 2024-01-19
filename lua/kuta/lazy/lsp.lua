c8e8ecabd2d1efc6b9b44fa9dc76fb7f2db033ea
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 11:53:37 +0100

    lsp.lua replaced

--- lua/kuta/lazy/lsp.lua
+++ lua/kuta/lazy/lsp.lua
@@ -1,68 +1,47 @@
-return{
+return {
     "neovim/nvim-lspconfig",
     dependencies = {
 
         --Snippets (don't even know what those are lmao)
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
 
         --Mason
         {
             "williamboman/mason.nvim",
             config = function()
                 require("mason").setup()
-            end
+            end,
         },
         {
             "williamboman/mason-lspconfig.nvim",
             config = function()
                 require("mason-lspconfig").setup({
                     ensure_installed = {
-                        'tsserver',
-                        'gradle_ls',
-                        'bashls',
-                        'cssls',
-                        'jdtls',
-                        'jedi_language_server',
-                        'jsonls',
-                        'kotlin_language_server',
-                        'lua_ls'
+                        "tsserver",
+                        "gradle_ls",
+                        "bashls",
+                        "cssls",
+                        "jdtls",
+                        "jedi_language_server",
+                        "jsonls",
+                        "kotlin_language_server",
+                        "lua_ls",
                     },
-                    handlers = {
-                        function(server_name)
-                            require("lspconfig")[server_name].setup{}
-                        end,
-                        ["lua_ls"] = function ()
-                            local lspconfig = require("lspconfig")
-                            lspconfig.lua_ls.setup {
-                                settings = {
-                                    Lua = {
-                                        diagnostics = {
-                                            globals = { "vim" }
-                                        }
-                                    }
-                                }
-                            }
-                        end,
-                        ["jdtls"] = function()
-                        end
-                    }
                 })
-            end
+            end,
         },
     },
     config = function()
-
-        local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "" }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-        end
-        vim.diagnostic.config{
-            virtual_text = true,
-            signs = {
-                active = signs,
-            },
+        local lspconfig = require("lspconfig")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+        local on_attach = function(client, bufnr) end
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_nvim_lsp.default_capabilities()
+        )
+        vim.diagnostic.config({
             update_in_insert = true,
             float = {
                 focusable = false,
@@ -71,15 +50,18 @@ return{
                 source = "always",
                 header = "",
                 prefix = "",
-            }
-        }
-        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-            border = "rounded",
+            },
         })
-
-        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-            border = "rounded",
+        lspconfig.lua_ls.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
         })
-    end
+    end,
 }
-
