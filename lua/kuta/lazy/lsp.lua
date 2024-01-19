8d78dc8b070cd0f7a19502dfa058f7e731551e1d
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 10:45:09 +0100

    Seperated cmp

--- lua/kuta/lazy/lsp.lua
+++ lua/kuta/lazy/lsp.lua
@@ -50,57 +50,19 @@ return{
                 })
             end
         },
-        'neovim/nvim-lspconfig',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        {
-            'hrsh7th/nvim-cmp',
-            config = function()
-
-                local cmp = require'cmp'
-                local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
-                cmp.setup({
-                    snippet = {
-                        expand = function(args)
-                            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                        end,
-                    },
-                    mapping = cmp.mapping.preset.insert({
-                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                        ['<C-Space>'] = cmp.mapping.complete(),
-                        ['<C-e>'] = cmp.mapping.abort(),
-                        ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                    }),
-                    sources = cmp.config.sources({
-                        { name = 'nvim_lsp' },
-                        { name = 'vsnip' }, -- For vsnip users.
-                        { name = 'luasnip' }, -- For luasnip users.
-                    }, {
-                        { name = 'buffer' },
-                    })
-                })
-            end
-
+    },
+    config = function()
+        vim.diagnostic.config{
+            update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            }
         }
-},
-config = function()
-vim.diagnostic.config{
-    update_in_insert = true,
-    float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-    }
-}
-end
+    end
 }
 
