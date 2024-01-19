df8432041d5941e3c37f518d0345520be8b7865e
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 11:00:14 +0100

    cmp still not quite there

--- lua/kuta/lazy/lsp.lua
+++ lua/kuta/lazy/lsp.lua
@@ -52,7 +52,17 @@ return{
         },
     },
     config = function()
+
+        local signs = { Error = "", Warn = "", Hint = "󰌵", Info = "" }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
         vim.diagnostic.config{
+            virtual_text = true,
+            signs = {
+                active = signs,
+            },
             update_in_insert = true,
             float = {
                 focusable = false,
@@ -63,6 +73,13 @@ return{
                 prefix = "",
             }
         }
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+            border = "rounded",
+        })
+
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+            border = "rounded",
+        })
     end
 }
 
